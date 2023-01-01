edd1797256f87e252248727c9b0fe002ad2330ca
Saif-Eddine MGAIDI <saif4547@gmail.com>
2023-01-01 15:19:04 +0100

    Modification questionnaire.py: Ajout fonctionnalité lancer questionnaire avec ligne de commande, re-structuration du code, nettoyage et ajouts de commentaires

--- questionnaire.py
+++ questionnaire.py
@@ -1,13 +1,43 @@
 import json
 import os
+import sys
 
-def demande_et_verification_du_fichier_json():
-    filename = input("questionnaires json: ")
+# Affiche les informations du questionnaire
+def afficher_infos_du_questionnaire(infos):
+    print()
+    print("Catégorie:", infos[0][0])
+    print("Titre:", infos[0][1])
+    print("Difficulté:", infos[0][2])
+    print("Nombres de Questions:", infos[0][3])
+    print()
+
+# Fonction qui vérifie l'existence du fichier
+def fichier_exist(filename):
     if os.path.exists(filename):
+        return True
+    else:
+        return False
+
+# Fonction qui demande à l'utilisateur de rentrer le fichier Json à charger
+def demande_du_fichier_json():
+    filename = input('Nom du fichier Json: ')
+    if fichier_exist(filename):
         return filename
     else:
-        print("Erreur: Le fichier est introuvable")
-        return demande_et_verification_du_fichier_json()
+        print('Erreur: le fichier Json est introuvable')
+        return demande_du_fichier_json()
+    
+# Prend en paramètre le nom du fichier
+# La fonction lit et récupère les données du fichier Json
+def lecture_et_recuperation_donnees_json(filename):
+    if not fichier_exist:
+        print('Erreur: Le fichier est introuvable')
+        return None
+    f = open(filename, "r")
+    data = json.load(f)
+    f.close()
+    return data
+
 
 
 class Question:
@@ -59,13 +89,12 @@ class Questionnaire:
 
     def lancer(self):
         score = 0
-
+    
         print("Bienvenue !")
         print("Retrouve ci-dessous, les informations du Quizz !")
-        print("Catégorie:", self.infos[0][0])
-        print("Titre:", self.infos[0][1])
-        print("Difficulté:", self.infos[0][2])
-        print("Nombres de Questions:", len(self.questions))
+
+        afficher_infos_du_questionnaire(self.infos)
+        
         print("A vous de jouer !")
         print()
         for i in range(0, len(self.questions)):
@@ -76,25 +105,33 @@ class Questionnaire:
         return score
 
 
-# Le fichier json est demandé à l'utilisateur, puis il est verifié
-filename = demande_et_verification_du_fichier_json()
+# Pour ligne de commande: Teste si un paramètre est passé en entrée
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+    # Vérifie le fichier passé en paramètre
+    if fichier_exist(filename):
+        pass
+    else:
+        print('Erreur: Le paramètre de la ligne de commande ne correspond à aucun fichier Json')
+        exit()
+else:
+    filename = demande_du_fichier_json()
 
-f = open(filename, "r")
-data = json.load(f)
-f.close()
 
-# On ajoute une question avec son titre et ses choix dans une liste qui regroupe toutes
-# les questions du fichier json
+# Variable qui récupère les données du fichier Json
+data = lecture_et_recuperation_donnees_json(filename)
 
-questionnaires_infos = [(data["categorie"], data["titre"], data["difficulte"])]
+# On ajoute les questions dans la liste questions
 questions = []
-
 for question in data["questions"]:
     titre = question["titre"]
     choix = question["choix"]
     questions.append(Question(titre, choix))
 
+# On ajoute aussi les infos concernant le questionnaire
+questionnaires_infos = [(data["categorie"], data["titre"], data["difficulte"], len(questions))]
 
 
-# On ne crée plus de question en dur, on passe directement la liste qui contient toutes les questions
+# On passe la liste des questions (premier paramètre)
+# On passe les informations du questionnaire (deuxième paramètre)
 Questionnaire(questions, questionnaires_infos).lancer()
